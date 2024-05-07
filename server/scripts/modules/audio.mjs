887557798cda868af6e7195a6d4057df313470ba
FoxxMD <FoxxMD@users.noreply.github.com>
2024-05-07 17:25:46 -0400

    Accept json for audio files and randomize/loop playlist

--- server/scripts/modules/audio.mjs
+++ server/scripts/modules/audio.mjs
@@ -4,10 +4,26 @@ async function getAudioFiles() {
 		if (!res.ok) {
 			throw new Error(`HTTP error. status: ${res.status}`);
 		}
-		let AUDIO_FILES_STRING = await res.text();
-		AUDIO_FILES_STRING = AUDIO_FILES_STRING.replace(/'+/g, '');
-		console.log('Audio listing:', AUDIO_FILES_STRING);
-		return AUDIO_FILES_STRING;
+		let AUDIO_FILES_STRING = '';
+		let isJson = res.headers.get('content-type').includes('json');
+		if(!isJson) {
+			AUDIO_FILES_STRING = await res.text();
+			AUDIO_FILES_STRING = AUDIO_FILES_STRING.replace(/'+/g, '');
+			if(!AUDIO_FILES_STRING.includes('.json')) {
+				console.log('Audio listing:', AUDIO_FILES_STRING);
+				return AUDIO_FILES_STRING;
+			} else {
+				const jsonRes = await (await fetch(AUDIO_FILES_STRING)).json()
+				console.log('Audio listing:');
+				console.log(jsonRes);
+				return jsonRes;
+			}
+		} else {
+			console.log('Audio listing:');
+			console.log(jsonRes);
+			return res.json();
+		}
+
 	} catch (error) {
 		console.error('Failed to fetch audio listing:', error);
 	}
@@ -23,18 +39,40 @@ const fisheryates = (arr) => {
 	return arr;
 };
 
+const randomIntFromInterval = (min, max) => { // min and max included
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 const setupAudio = () => {
 	getAudioFiles().then((data) => {
-		const AUDIO_FILES = fisheryates(data.split(','));
+		let AUDIO_FILES = [];
 
-		let index = 0;
+		if(Array.isArray(data)) {
+			AUDIO_FILES = data;
+		} else {
+			AUDIO_FILES = fisheryates(data.split(','));
+		}
+
+		if(AUDIO_FILES.length === 0) {
+			return;
+		}
+
+		let index = randomIntFromInterval(0, AUDIO_FILES.length);
+		let alreadyPlayed = [index];
 
 		const audio = document.querySelector('#twcAudio');
 		audio.src = AUDIO_FILES[index];
 
 		audio.addEventListener('ended', () => {
-			index = index > AUDIO_FILES.length ? 0 : index + 1;
-			audio.src = AUDIO_FILES[index];
+			if(alreadyPlayed.length === AUDIO_FILES.length) {
+				alreadyPlayed = [];
+			}
+			let newIndex = randomIntFromInterval(0, AUDIO_FILES.length);
+			while(alreadyPlayed.includes(newIndex)) {
+				newIndex = randomIntFromInterval(0, AUDIO_FILES.length);
+			}
+			audio.src = AUDIO_FILES[newIndex];
+			alreadyPlayed.push(newIndex)
 			audio.play();
 		});
 	});
